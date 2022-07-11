dc29320f0ca2f718014da195c61c7fe11851fffc
Ruslan Mansurov <ruslanways@gmail.com>
2022-07-11 17:59:26 +0200

    added homepage with tepmlates and first view

--- blogspot-project/diary/views.py
+++ blogspot-project/diary/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
-# Create your views here.
+class HomeView(ListView):
+    template_name = 'diary/index.html'
+    model = Post
+
