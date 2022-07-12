694a1c3ae86c15eab6edcb7f666cd2173ddbff9f
Ruslan Mansurov <ruslanways@gmail.com>
2022-07-12 11:58:34 +0200

    added logo and PostDetailView

--- blogspot-project/diary/urls.py
+++ blogspot-project/diary/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('<pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
-
--- blogspot-project/diary/views.py
+++ blogspot-project/diary/views.py
@@ -1,5 +1,6 @@
+from pyexpat import model
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post
 from django.db.models import Count
 
@@ -7,4 +8,8 @@ class HomeView(ListView):
     template_name = 'diary/index.html'
     queryset = Post.objects.annotate(Count('like')).select_related('author')
     ordering = ['-like__count']
+
+
+class PostDetailView(DetailView):
+    queryset = Post.objects.annotate(Count('like')).select_related('author')
 
