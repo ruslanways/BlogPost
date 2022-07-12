f546237e4a19c4330d0ba0648c530209ebe1b9e8
Ruslan Mansurov <ruslanways@gmail.com>
2022-07-12 20:07:45 +0200

    added authors and authors-detail views and templates

--- blogspot-project/diary/urls.py
+++ blogspot-project/diary/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView, PostDetailView
+from .views import HomeView, PostDetailView, AuthorDetailView, AuthorListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('<pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('authors/', AuthorListView.as_view(), name='author-list'),
+    path('authors/<pk>/', AuthorDetailView.as_view(), name='author-detail'),
 ]
--- blogspot-project/diary/views.py
+++ blogspot-project/diary/views.py
@@ -1,7 +1,9 @@
+from multiprocessing import context
 from pyexpat import model
+from urllib import request
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
-from .models import Post
+from .models import Post, CustomUser
 from django.db.models import Count
 
 class HomeView(ListView):
@@ -12,4 +14,16 @@ class HomeView(ListView):
 
 class PostDetailView(DetailView):
     queryset = Post.objects.annotate(Count('like')).select_related('author')
+
+
+class AuthorListView(ListView):
+    model = CustomUser
+
+
+class AuthorDetailView(DetailView):
+    queryset = CustomUser.objects.all().prefetch_related('post_set')
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['likes'] = Post.objects.annotate(Count('like')).filter(author__pk=self.object.pk).select_related('author')
+    #     return context
 
