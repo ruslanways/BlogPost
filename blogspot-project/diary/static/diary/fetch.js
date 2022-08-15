ffbd7e349900b03ba89ed830236c871e5f60bfd2
Ruslan Mansurov <ruslanways@gmail.com>
2022-08-15 20:49:13 +0200

    removed id attribute in .like and in image link as no needed with fetch

--- blogspot-project/diary/static/diary/fetch.js
+++ blogspot-project/diary/static/diary/fetch.js
@@ -4,7 +4,8 @@ likes.forEach((like) => {
   like.addEventListener("click", async function (evt) {
     evt.preventDefault();
     await fetch(this.getAttribute("href"));
-    let content = this.innerHTML.split(" ");
+    let content = this.innerHTML.trim().split(" ");
+    console.log(content);
     if (content[0].charCodeAt(0) === 10084) {
       content[0] = "&#9825;";
       content[1] = --content[1];
