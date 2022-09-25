4519e95ee453c16806b7272733079b05a4f29090
Ruslan Mansurov <ruslanways@gmail.com>
2022-09-25 22:36:21 +0200

    refactored test

--- blogspot-project/diary/tests.py
+++ blogspot-project/diary/tests.py
@@ -1,15 +1,10 @@
 from pprint import pprint
-from django.urls import resolve
 from .serializers import PostsSerializer
 from .models import CustomUser, Post
-from django.test import TestCase
-from django.urls import reverse
-from rest_framework.test import APITestCase, APIRequestFactory
-from rest_framework.request import Request
-from rest_framework import serializers
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
 from rest_framework.settings import api_settings
-from unittest import mock
-from django.test.client import RequestFactory
+
 
 
 class PostsTestCase(APITestCase):
@@ -24,22 +19,19 @@ class PostsTestCase(APITestCase):
         test_post_3 = Post.objects.create(title='TestPost3', author= test_user_1, content='Some test 3 content')
         test_post_4 = Post.objects.create(title='TestPost4', author= test_user_2, content='Some test 4 content')
         test_post_5 = Post.objects.create(title='TestPost5', author= test_user_3, content='Some test 5 content')
+        test_post_6 = Post.objects.create(title='TestPost6', author= test_user_3, content='Some test 6 content')
+        test_post_7 = Post.objects.create(title='TestPost7', author= test_user_1, content='Some test 7 content')
+        test_post_8 = Post.objects.create(title='TestPost8', author= test_user_2, content='Some test 8 content')
+        test_post_9 = Post.objects.create(title='TestPost9', author= test_user_3, content='Some test 9 content')
+        test_post_10 = Post.objects.create(title='TestPost10', author= test_user_2, content='Some test 10 content')
+        test_post_11 = Post.objects.create(title='TestPost11', author= test_user_3, content='Some test 11 content')
 
         queryset = Post.objects.all()
 
-        # here we use APIRequestFactory because we need an HttpRequest object to build uri for Hyperlinked serializer
-        request = APIRequestFactory().get(reverse('post-list-api'))
+        response = self.client.get(reverse('post-list-api'))
 
-        # then we call appropriate view to test the api what server by that request - i.e. view
-        response = resolve(reverse('post-list-api')).func(request)
+        serializer = PostsSerializer(queryset, many=True, context={'request': response.wsgi_request})
 
-        # or we can use client from APITestCase to mimic browser-client request
-        #response = self.client.get(reverse('post-list-api'))
-
-        serializer = PostsSerializer(queryset, many=True, context={'request': request})
-
-        if api_settings.DEFAULT_PAGINATION_CLASS:
-            self.assertEqual(serializer.data, response.data['results'])
-        else:
-            self.assertEqual(serializer.data, response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(serializer.data[:api_settings.PAGE_SIZE], response.data['results'])
 
