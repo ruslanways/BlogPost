e63412e4d36bc8e5f599853fd03b625162f1c174
Ruslan Mansurov <ruslanways@gmail.com>
2022-10-13 19:38:43 +0200

    refactored fetch.js with more readable code

--- blogspot-project/diary/static/diary/fetch.js
+++ blogspot-project/diary/static/diary/fetch.js
@@ -3,39 +3,45 @@ using fetch-api to like posts
 by making GET-requests to Django LikeCreateView
 */
 
+// Get all like <div>'s elements to operate with
 const likes = document.querySelectorAll(".like");
 
+// Send a get request to add or delete like
+// Update a color and number of heart
+async function makeLike(event) {
+  event.preventDefault();
+  await fetch(this.href); // 'this' reffers to as addEventListener object
+  // Get current heart+number
+  let content = this.innerHTML.trim().split(" ");
+  // Change hear to opposite color and increment or decrement number
+  if (content[0].charCodeAt(0) === 10084) {
+    content[0] = "&#9825;";
+    --content[1];
+  } else {
+    content[0] = "&#10084;";
+    ++content[1];
+  }
+  this.innerHTML = `${content[0]} ${content[1]}`;
+}
+
+// Add addEventListener to .like element
+let readyForLike = function (like) {
+  like.addEventListener("click", makeLike);
+};
+
+// Add addEventListener on all .like elements
+let onLikeClick = function () {
+  likes.forEach(readyForLike);
+};
+
+// If the user is authenticated - start likes functionality
 if (document.getElementById("user")) {
-  likes.forEach(like => {
-    like.addEventListener("click", async function(evt) {
-      evt.preventDefault();
-      await fetch(this.getAttribute("href"));
-
-      // await fetch(this.getAttribute("href"), {
-      //   method: "POST",
-      //   body: JSON.stringify({
-      //     'post_id': this.getAttribute("href").slice(11,-1),
-      //   }),
-      // }
-      // );
-
-      let content = this.innerHTML.trim().split(" ");
-      if (content[0].charCodeAt(0) === 10084) {
-        content[0] = "&#9825;";
-        --content[1];
-      } else {
-        content[0] = "&#10084;";
-        ++content[1];
-      }
-      this.innerHTML = `${content[0]} ${content[1]}`;
-    });
-  });
+  onLikeClick();
 }
 
 
 
-// dynamically updates likes with interval of 3 sec.
-
+// Dynamically updates likes
 let updateLike = () => {
   likes.forEach(async function (like) {
     const old_like = like.innerHTML.trim();
@@ -45,9 +51,9 @@ let updateLike = () => {
     const data = await response.text();
     if (old_like !== data) {
       like.innerHTML = data;
-    } 
     }
-  );
+  });
 };
 
+// Updates likes with interval of 3 sec
 setInterval(updateLike, 3000);
