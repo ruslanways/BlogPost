64dce0eaed50bc9e4a39d5a8a263ff541e50adb0
Ruslan Mansurov <ruslanways@gmail.com>
2022-10-22 08:55:53 +0200

    renamed project name to blogpost-project; renamed main project director to blogpost

--- blogpost-project/blogpost/urls.py
+++ blogpost-project/blogpost/urls.py
@@ -1,4 +1,4 @@
-"""blogspot URL Configuration
+"""blogpost URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- blogpost-project/blogpost/wsgi.py
+++ blogpost-project/blogpost/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for blogspot project.
+WSGI config for blogpost project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogspot.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogpost.settings')
 
 application = get_wsgi_application()
