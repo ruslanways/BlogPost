f082ad42dda8ce96567d314121bccc754459e05a
Ruslan Mansurov <ruslanways@gmail.com>
2022-10-26 16:33:04 +0200

    added www.postways.net to allowed hosts

--- blogpost-project/blogpost/settings.py
+++ blogpost-project/blogpost/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['postways.net', 'localhost']
+ALLOWED_HOSTS = ['postways.net', 'www.postways.net', 'localhost']
 
 APPEND_SLASH = False
 
