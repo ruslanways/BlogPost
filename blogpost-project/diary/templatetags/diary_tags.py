ce3b505d1cf09ecc15ea480b7b639b84e4430a9d
Ruslan Mansurov <ruslanways@gmail.com>
2022-10-27 14:38:53 +0200

    added new template filter url_target_blank

--- blogpost-project/diary/templatetags/diary_tags.py
+++ blogpost-project/diary/templatetags/diary_tags.py
@@ -9,3 +9,6 @@ register = template.Library()
 def like_or_unlike(author, post):
     return '&#10084;' if author.like_set.all() & post.like_set.all() else '&#9825;'
 
+@register.filter(is_safe=True)
+def url_target_blank(text):
+    return text.replace('<a ', '<a target="_blank" ')
