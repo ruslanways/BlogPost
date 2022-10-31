b54079915f5cfd0ff216a4ce40b2b93b81cd7626
Ruslan Mansurov <ruslanways@gmail.com>
2022-10-31 10:36:01 +0100

    corrected beat_schedule

--- blogpost-project/blogpost/celery.py
+++ blogpost-project/blogpost/celery.py
@@ -11,7 +11,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'week-report': {
         'task': 'diary.tasks.send_week_report',
-        'schedule': crontab(hour=9, minute=40, day_of_week=0),
+        'schedule': crontab(hour=9, minute=40, day_of_week=1),
     },
 }
 
