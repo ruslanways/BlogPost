56c3eeeef9758b578731bb9d6418dcca885af0ae
Ruslan Mansurov <ruslanways@gmail.com>
2023-03-14 15:42:37 +0100

    added resize.js

---
+++ blogpost-project/diary/static/diary/resize.js
@@ -0,0 +1,60 @@
+document.getElementById('createpost').addEventListener('click', function (e) {
+    e.preventDefault();
+    resizeImage();
+});
+
+function resizeImage() {
+    const fileInput = document.getElementById('id_image');
+    const file = fileInput.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = function (event) {
+        const img = new Image();
+        img.src = event.target.result;
+        img.onload = function () {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            const MAX_WIDTH = 800;
+            const MAX_HEIGHT = 600;
+            let width = img.width;
+            let height = img.height;
+
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(img, 0, 0, width, height);
+
+            const resizedFile = canvas.toDataURL('image/jpeg', 0.7);
+            const newFile = dataURLtoFile(resizedFile, file.name);
+            fileInput.files[0] = newFile;
+
+            document.getElementById('myForm').submit();
+        };
+    };
+}
+
+function dataURLtoFile(dataurl, filename) {
+    const arr = dataurl.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+}
